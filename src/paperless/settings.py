717da77b63bb69f5bf0adcbf8853bf5987815365
Trenton H <797416+stumpylog@users.noreply.github.com>
2024-01-29 11:59:02 -0800

    Include the ocrmypdf logging into the log file

--- src/paperless/settings.py
+++ src/paperless/settings.py
@@ -7,7 +7,6 @@ import re
 import tempfile
 from os import PathLike
 from pathlib import Path
-from platform import machine
 from typing import Final
 from typing import Optional
 from typing import Union
@@ -358,10 +357,7 @@ ASGI_APPLICATION = "paperless.asgi.application"
 STATIC_URL = os.getenv("PAPERLESS_STATIC_URL", BASE_URL + "static/")
 WHITENOISE_STATIC_PREFIX = "/static/"
 
-if machine().lower() == "aarch64":  # pragma: no cover
-    _static_backend = "django.contrib.staticfiles.storage.StaticFilesStorage"
-else:
-    _static_backend = "whitenoise.storage.CompressedStaticFilesStorage"
+_static_backend = "django.contrib.staticfiles.storage.StaticFilesStorage"
 
 STORAGES = {
     "staticfiles": {
@@ -700,6 +696,7 @@ LOGGING = {
         "paperless_mail": {"handlers": ["file_mail"], "level": "DEBUG"},
         "celery": {"handlers": ["file_celery"], "level": "DEBUG"},
         "kombu": {"handlers": ["file_celery"], "level": "DEBUG"},
+        "ocrmypdf": {"handlers": ["file_paperless"], "level": "DEBUG"},
     },
 }
 
